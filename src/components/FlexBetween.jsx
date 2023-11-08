00fb554d02d11b13458d9d0e127bfe775bd2cc95
Shivanshu Bhargava <shivanshu.bhargava@capgemini.com>
2023-11-08 17:11:53 +0530

    code changes for the new working UI

--- src/components/FlexBetween.jsx
+++ src/components/FlexBetween.jsx
@@ -1,6 +1,5 @@
-import { Box } from "@mui/material";
-import { styled } from "@mui/system";
-
+const { Box } = require("@mui/material");
+const { styled } = require("@mui/system");
 
 const FlexBetween = styled(Box)({
   display: "flex",
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import FlexBetween from "components/FlexBetween";
+import Header from "components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import BreakdownChart from "components/BreakdownChart";
+import OverviewChart from "components/OverviewChart";
+import { useGetUserQuery } from "state/api";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const {data } = useGetUserQuery();
+  console.log(data);
+  const columns = [
+    {
+      field: "_id",
+      headerName: "Device ID",
+      flex: 0.5,
+    },
+    {
+      field: "name",
+      headerName: "User",
+      flex: 0.4,
+    },
+    {
+      field: "email",
+      headerName: "email",
+      flex: 0.4,
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 0.4,
+      
+    },
+    {
+      field: "profile_exist",
+      headerName: "Profile exist",
+      flex: 0.4,
+    },
+    {
+      field: "roles",
+      headerName: "Role",
+      flex: 0.3,
+    },
+    {
+      field: "role",
+      headerName: "BP",
+      flex: 0.3,
+    },
+   
+    
+    
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        {/* <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box> */}
+      </FlexBetween>
+
+      <Box
+        mt="20px"
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="160px"
+        gap="20px"
+        sx={{
+          "& > div": { gridColumn: isNonMediumScreens ? undefined : "span 12" },
+        }}
+      >
+        {/* ROW 1 */}
+        
+        
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={theme.palette.background.alt}
+          p="1rem"
+          borderRadius="0.55rem"
+        >
+          {/* <OverviewChart view="sales" isDashboard={true} /> */}
+        </Box>
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={theme.palette.background.alt}
+          p="1.5rem"
+          borderRadius="0.55rem"
+        >
+          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+            Activity
+          </Typography>
+          {/* <BreakdownChart isDashboard={true} /> */}
+          <Typography
+            p="0 0.6rem"
+            fontSize="0.8rem"
+            sx={{ color: theme.palette.secondary[200] }}
+          >
+            
+          </Typography>
+        </Box>
+        
+
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 12"
+          gridRow="span 3"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              borderRadius: "5rem",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme.palette.background.alt,
+            },
+            "& .MuiDataGrid-footerContainer": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderTop: "none",
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${theme.palette.secondary[200]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            // loading={isLoading || !data}
+            getRowId={(row) => row._id}
+            rows={(data) || []}
+            columns={columns}
+          />
+        </Box>
+        
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
