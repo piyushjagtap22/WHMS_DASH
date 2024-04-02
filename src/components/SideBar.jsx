1d9494cc312211abfc648b3341f2cf243695aab3
harsh <harsh@harshs-MacBook-Air.local>
2024-04-02 18:02:46 +0530

    cosmetic changes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -37,7 +37,7 @@ import {
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import FlexBetween from './FlexBetween';
-import profileImage from '../assets/profile.jpeg';
+import profileImage from '../assets/profile.png';
 
 const navItems = [
   {
