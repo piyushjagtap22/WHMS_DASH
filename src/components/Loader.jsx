f961ee81227c1e0d8b46b8a38c2f9bf6c18c7363
piyushjagtap22 <jagspiyush@gmail.com>
2024-08-09 02:21:20 +0530

    Putting circular progress at loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Loader = () => {
   return (
@@ -8,17 +9,20 @@ const Loader = () => {
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh' /* Set height to 100% of viewport height */,
+        color: '#7CD6AB',
       }}
     >
-      <img
+      {/* <img
         src='/src/assets/loader.gif'
         alt='Loading...'
         style={{
-          width: '100px' /* Adjust the width as needed */,
+          width: '100px' /* Adjust the width as needed 
           height: '100px',
-          scale: '2' /* Adjust the height as needed */,
+          scale: '2' /* Adjust the height as needed 
         }}
-      />
+      /> */}
+
+      <CircularProgress color='inherit' />
     </div>
   );
 };
