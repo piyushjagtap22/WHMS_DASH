3991e659f0b73f4dc5b267efb5bc3db1208b191f
piyushjagtap22 <jagspiyush@gmail.com>
2024-08-09 01:22:50 +0530

    loader path updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh', /* Set height to 100% of viewport height */
-    }}>
-      <img src="../src/assets/loader.gif" alt="Loading..." style={{
-        width: '100px', /* Adjust the width as needed */
-        height: '100px', 
-        scale: '2'/* Adjust the height as needed */
-      }} />
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh' /* Set height to 100% of viewport height */,
+      }}
+    >
+      <img
+        src='../assets/loader.gif'
+        alt='Loading...'
+        style={{
+          width: '100px' /* Adjust the width as needed */,
+          height: '100px',
+          scale: '2' /* Adjust the height as needed */,
+        }}
+      />
     </div>
   );
 };
